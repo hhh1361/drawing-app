52be3b76f167eac7a2a0d821a3b212aabc4a083e
hhh1361 <nikita.turlyko@gmail.com>
2019-12-24 18:50:36 +0300

    Updates

---
+++ precache-manifest.3bb8ede5dc73fccea28687bf278e232c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d341ab6f39051ca2fae93ed134b3c52c",
+    "url": "/drawing-app/index.html"
+  },
+  {
+    "revision": "fa53c9612ac70c5a566f",
+    "url": "/drawing-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b958b39dc045113e1830",
+    "url": "/drawing-app/static/css/main.475fde8b.chunk.css"
+  },
+  {
+    "revision": "fa53c9612ac70c5a566f",
+    "url": "/drawing-app/static/js/2.0f0e9112.chunk.js"
+  },
+  {
+    "revision": "b958b39dc045113e1830",
+    "url": "/drawing-app/static/js/main.25590436.chunk.js"
+  },
+  {
+    "revision": "8b8c948a5a92854dc233",
+    "url": "/drawing-app/static/js/runtime-main.032b0828.js"
+  }
+]);
